1202db40ac260cf10324e8ce6c3ff4f4ec24a941
Del Edoghotu <edoghotudeliverance@gmail.com>
2023-02-20 15:51:16 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
- *main - entry point
- *return: always 0 (suxxess)
+ * main - Entry point
+ * Description: '4-puts'
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return (0);
+	puts("/"Programming is like building a multilingual puzzle");
+	return (0);
 }
