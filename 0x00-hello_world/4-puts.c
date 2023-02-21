452d4cf265362b4feb5837f68f4735b1581d26bf
Del Edoghotu <edoghotudeliverance@gmail.com>
2023-02-20 16:06:26 -0800

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: '4-puts'
+ * Description: 'Using the function puts'
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	puts("/"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
